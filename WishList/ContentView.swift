5a19cb4822991346c5d65978e7af0f97769d9044
AndersonPS <dson_adn@hotmail.com>
2025-08-24 10:25:25 -0300

    Initial Commit

---
+++ WishList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WishList
+//
+//  Created by Anderson Pereira Dos Santos on 24/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WishList/WishListApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishListApp.swift
+//  WishList
+//
+//  Created by Anderson Pereira Dos Santos on 24/08/25.
+//
+
+import SwiftUI
+
+@main
+struct WishListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
